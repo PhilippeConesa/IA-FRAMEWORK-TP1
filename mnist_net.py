40c02ec2d71194be9965bac147030db118702756
PhilippeConesa <philippe.conesa.pc@gmail.com>
2022-09-26 15:54:50 +0200

    TP1

--- mnist_net.py
+++ mnist_net.py
@@ -9,23 +9,34 @@ import torch.nn.functional as F
 class MNISTNet(nn.Module):
     def __init__(self):
         super(MNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(...)
-        self.conv2 = nn.Conv2d(...)
-        self.pool = nn.MaxPool2d(...)
-        self.fc1 = nn.Linear(...)
-        self.fc2 = nn.Linear(...)
-        self.fc3 = nn.Linear(...)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=(5, 5))
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(5, 5))
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+
+        self.linear1 = nn.Linear(in_features=16 * 4 * 4, out_features=128)
+        self.linear2 = nn.Linear(in_features=128, out_features=64)
+        self.linear3 = nn.Linear(in_features=64, out_features=10)
+
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
         x = F.relu(self.conv1(x))       # First convolution followed by
         x = self.pool(x)                # a relu activation and a max pooling#
-        x = ...
-        ...
-        x = self.fc3(x)
+        x = F.relu(self.conv2(x)) 
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = F.relu(self.linear1(x))
+        x= F.relu(self.linear2(x))
+        x=self.linear3(x)
         return x
 
     def get_features(self, x):
-        pass
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        return x
 
 
 if __name__=='__main__':
